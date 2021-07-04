34a8e1d08207fa541e86f4c85cebfbbb48f42822
Abhishek Patel <ap8209655@gmail.com>
2021-07-04 12:14:04 +0530

    Change in mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 const env = require("./environment");
-mongoose.connect(`mongodb://localhost/${env.db}`);
-
+mongoose.connect(
+  "mongodb+srv://wbpractice:abhi@123@cluster0.ghnsc.mongodb.net/wbpractice?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
